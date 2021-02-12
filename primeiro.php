5ebd05fd8e683bb17f2b29f26bc06897b58dffe8
yurisa2 <yuri@sa2.com.br>
2021-02-12 20:29:24 -0300

    Create primeiro.php

---
+++ primeiro.php
@@ -0,0 +1,11 @@
+Este é o nosso primeiro arquivo PHP.
+A primeira vez é inesquecível.
+
+<?php
+// https://www.php.net/manual/pt_BR/function.echo.php
+
+
+echo 'Isso está dentro da tag php.';
+
+
+ ?>
